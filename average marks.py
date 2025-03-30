5bd9cf21ad2fe5525db0f8afa31ab30576d5d99e
VivasvanVerma <vvofficial.yt@gmail.com>
2025-03-30 12:36:53 +0530

    Marks

---
+++ average marks.py
@@ -0,0 +1,7 @@
+a = int(input("Enter math marks: "))
+b = int(input("Enter science marks: "))
+c = int(input("Enter hindi marks: "))
+d = int(input("Enter english marks: "))
+e = int(input("Enter history marks: "))
+avr = (a+b+c+d+e)/5
+print("Average marks: ", avr)
