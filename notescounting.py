974051fda3b2b575c89a9390985b68c8feda2a56
VivasvanVerma <vvofficial.yt@gmail.com>
2025-03-30 12:48:08 +0530

    notes counting

---
+++ notescounting.py
@@ -0,0 +1,14 @@
+amount = int(input("Enter a sum of money: "))
+a = amount//500
+a1 = amount % 500
+b = a1//100
+b1 = a1 % 100
+c = b1//50
+c1 = b1 % 50
+d = c1//10
+d1 = c1 % 10
+print("The number of 500 rs notes: ", a)
+print("The number of 100 rs notes: ", b)
+print("The number of 50 rs notes: ", c)
+print("The number of 10 rs notes: ", d)
+
